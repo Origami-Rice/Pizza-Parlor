91bac1f29788f2e05732df2cc02967dcbf37b0ca
william.chu001@gmail.com <william.chu001@gmail.com>
2020-11-06 20:21:25 -0500

    add more test cases

--- PizzaParlour.py
+++ PizzaParlour.py
@@ -8,6 +8,8 @@ CORS(app)
 #
 # This function to retrieve last order number
 # and return a new order number
+
+
 def find_last_order_no():
     with open('order/last_order_no', 'r') as f:
         last_no = f.readline()
@@ -19,21 +21,26 @@ def find_last_order_no():
     return str(new_no)
 
 # part of starter code
+
+
 @app.route('/pizza')
 def welcome_pizza():
     return 'Welcome to Pizza Planet!'
 
-@app.route('/create_menu', methods=['POST', 'GET'])
+
+""" @app.route('/create_menu', methods=['POST', 'GET'])
 def create_menu():
     jsonData = request.get_json()
     order_file_name = 'order/' + 'Menu.json'
     with open(order_file_name, 'w') as outfile:
         json.dump(jsonData, outfile)
-    return jsonData 
+    return jsonData  """
 
 # Persist a new order on backend data storage
 # in this project, it's just stored under /order
 # directory as a system file
+
+
 @app.route('/create', methods=['POST', 'GET'])
 def create_order():
     new_order_no = find_last_order_no()
@@ -44,6 +51,8 @@ def create_order():
     return new_order_no
 
 # Retrieve an order from backend data storage by an order number
+
+
 @app.route('/retrieve/<string:order_no>', methods=['GET'])
 def retrieve_order(order_no):
     order_file_name = 'order/' + order_no + '.json'
@@ -56,6 +65,8 @@ def retrieve_order(order_no):
         return "ERROR: order - " + order_no + " not found!"
 
 # Delete an order from backend data storage by an order number
+
+
 @app.route('/delete/<string:order_no>', methods=['GET'])
 def delete_order(order_no):
     order_file_name = 'order/' + order_no + '.json'
@@ -66,6 +77,7 @@ def delete_order(order_no):
         # although order file is not found, it's still treated as
         # delete successfully
         return order_no
+
 
 @app.route('/update/<string:order_no>', methods=['POST', 'GET'])
 def update_order(order_no):
@@ -78,7 +90,6 @@ def update_order(order_no):
         return order_no
     else:
         return "ERROR: order - " + order_no + " not found!"
-
 
 
 if __name__ == "__main__":
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,7 +1,91 @@
 from PizzaParlour import app
+import json
+
 
 def test_pizza():
     response = app.test_client().get('/pizza')
 
-    assert response.status_code == 500
+    assert response.status_code == 200
     assert response.data == b'Welcome to Pizza Planet!'
+
+
+def test_create():
+    with open('order/last_order_no', 'r') as f:
+        last_no = f.readline()
+        new_no = int(last_no) + 1
+        order_no = str(new_no)
+        f.close()
+    with app.test_client() as client:
+        # send data as POST form to endpoint
+        sent = {"order_number": "1", "items": [{"type": "pepperoni", "size": 12, "topping": [
+            "olives", "tomatoes", "olives", "tomatoes"], "price": 14}]}
+        result = client.post(
+            '/create',
+            data=sent
+        )
+        # check result from server with expected data
+        print(result.data.decode('utf-8'))
+        assert result.data.decode('utf-8') == str(new_no)
+
+
+def test_retrieve():
+    with open('order/last_order_no', 'r') as f:
+        last_no = f.readline()
+        f.close()
+    response = app.test_client().get('/retrieve/'+last_no)
+    print("order_no: " + last_no)
+    assert response.status_code == 200
+
+
+def test_update():
+    with open('order/last_order_no', 'r') as f:
+        last_no = f.readline()
+        f.close()
+    with app.test_client() as client:
+        # send data as POST form to endpoint
+        sent = {"order_number": "1", "items": [{"type": "pepperoni", "size": 12, "topping": [
+            "olives", "tomatoes", "olives", "tomatoes"], "price": 14}]}
+        response = client.post(
+            '/update/'+last_no,
+            data=sent
+        )
+    assert response.status_code == 200
+    print(response.data.decode('utf-8'))
+    assert response.data.decode('utf-8') == last_no
+
+
+def test_delete():
+    with open('order/last_order_no', 'r') as f:
+        last_no = f.readline()
+        f.close()
+    response = app.test_client().get('/delete/'+last_no)
+    assert response.status_code == 200
+
+
+def test_delete_not_found():
+    response = app.test_client().get('/delete/1000')
+    assert response.status_code == 200
+    print(response.data.decode('utf-8'))
+    assert response.data.decode('utf-8') == "1000"
+
+
+def test_retrieve_not_found():
+    response = app.test_client().get('/retrieve/1000')
+
+    assert response.status_code == 200
+    print(response.data.decode('utf-8'))
+    assert response.data.decode('utf-8') == "ERROR: order - 1000 not found!"
+
+
+def test_update_not_found():
+    with app.test_client() as client:
+        # send data as POST form to endpoint
+        sent = {"order_number": "1", "items": [{"type": "pepperoni", "size": 12, "topping": [
+            "olives", "tomatoes", "olives", "tomatoes"], "price": 14}]}
+        response = client.post(
+            '/update/1000',
+            data=sent
+        )
+    assert response.status_code == 200
+    print(response.data.decode('utf-8'))
+    assert response.data.decode('utf-8') == "ERROR: order - 1000 not found!"
