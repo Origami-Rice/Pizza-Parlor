7a512b5fdcce9c9d5cd56fcebacc1bdba782fed9
xinyuan0801 <w596323108y@gmail.com>
2020-11-12 14:58:39 +0800

    tests added for Order class

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -103,6 +103,9 @@ class TestPizzaClass(unittest.TestCase):
     def test_init(self):
         pizza = Pizza("pepperoni", 12, 1)
         self.assertEqual(pizza.getPrice(), 10)
+        self.assertEqual(pizza.getSize(), 12)
+        self.assertEqual(pizza.getType(), "pepperoni")
+        self.assertEqual(pizza.getQuantity(), 1)
 
     def test_addEmptyTopping(self):
         pizza = Pizza("pepperoni", 12, 1)
@@ -142,12 +145,191 @@ class TestPizzaClass(unittest.TestCase):
                          ["olives", "tomatoes", "olives", "mushrooms"])
         self.assertEqual(pizza.getPrice(), 27.0)
 
+    def test_pizzachangeSize(self):
+        pizza = Pizza("pepperoni", 12, 1)
+        pizza.changeSize(15)
+        self.assertEqual(pizza.getSize(), 15)
+        self.assertEqual(pizza.getPrice(), 15)
+
+    def test_pizzachangeSizeMultipleQuantity(self):
+        pizza = Pizza("pepperoni", 12, 2)
+        pizza.changeSize(15)
+        self.assertEqual(pizza.getSize(), 15)
+        self.assertEqual(pizza.getPrice(), 30)
+
+    def test_pizzaChangeType(self):
+        pizza = Pizza("pepperoni", 12, 1)
+        pizza.changeType("margherita")
+        self.assertEqual(pizza.getType(), "margherita")
+        self.assertEqual(pizza.getTopping(), ["olives", "tomatoes"])
+        self.assertEqual(pizza.getPrice(), 11)
+
+    def test_pizzaChangeTypeMultipleQuantity(self):
+        pizza = Pizza("pepperoni", 12, 2)
+        pizza.changeType("margherita")
+        self.assertEqual(pizza.getType(), "margherita")
+        self.assertEqual(pizza.getTopping(), ["olives", "tomatoes"])
+        self.assertEqual(pizza.getPrice(), 22)
+
+    def test_pizzaGetTopping(self):
+        pizza = Pizza("pepperoni", 12, 1)
+        self.assertEqual(pizza.getTopping(), ["olives", "tomatoes"])
+
+    def test_pizzaGetQuantity(self):
+        pizza = Pizza("pepperoni", 12, 1)
+        self.assertEqual(pizza.getQuantity(), 1)
+
+    def test_pizzaGetSiza(self):
+        pizza = Pizza("pepperoni", 12, 1)
+        self.assertEqual(pizza.getSize(), 12)
+
+    def test_pizzaChangeTopping(self):
+        pizza = Pizza("pepperoni", 12, 1)
+        pizza.changeTopping(["olives"])
+        self.assertEqual(pizza.getTopping(), ["olives"])
+        self.assertEqual(pizza.getPrice(), 8)
+
+    def test_pizzaChangeQuantity(self):
+        pizza = Pizza("pepperoni", 12, 1)
+        pizza.changeQuantity(3)
+        self.assertEqual((pizza.getPrice()), 30)
+
+    def test_pizzaChangeToppingMultipleQuantity(self):
+        pizza = Pizza("pepperoni", 12, 2)
+        pizza.changeTopping(["olives"])
+        self.assertEqual(pizza.getTopping(), ["olives"])
+        self.assertEqual(pizza.getPrice(), 16)
+
+    def test_getMenu(self):
+        pizza = Pizza("pepperoni", 12, 1)
+        menu = pizza.getMenu()
+        self.assertEqual(menu, {
+            "drinks": {
+                "Coke": 2.5,
+                "Diet Coke": 3,
+                "Coke Zero": 3,
+                "Pepsi": 2.5,
+                "Diet Pepsi": 3,
+                "Dr. Pepper": 4,
+                "Water": 1,
+                "Juice": 2
+            },
+            "pizza": {
+                "Type": {
+                    "pepperoni": [
+                        [
+                            "olives",
+                            "tomatoes"
+                        ],
+                        10,
+                        15,
+                        20
+                    ],
+                    "margherita": [
+                        [
+                            "olives",
+                            "tomatoes"
+                        ],
+                        11,
+                        15,
+                        20
+                    ],
+                    "vegetarian": [
+                        [
+                            "olives",
+                            "tomatoes"
+                        ],
+                        10,
+                        15,
+                        20
+                    ],
+                    "Neapolitan": [
+                        [
+                            "olives",
+                            "tomatoes"
+                        ],
+                        10,
+                        15,
+                        20
+                    ]
+                },
+                "Toppings": {
+                    "olives": 2,
+                    "tomatoes": 2,
+                    "mushrooms": 1.5,
+                    "jalapenos": 2,
+                    "chicken": 5,
+                    "beef": 6,
+                    "pepperoni": 6
+                }
+            }
+        })
+
+
 class TestDrinksClass(unittest.TestCase):
 
     def test_init(self):
-        drink = Drinks("Coke", 1)
-        self.assertEqual(drink.getPrice(), 2.5)
+        drink = Drinks("Coke", 2)
+        self.assertEqual(drink.getPrice(), 5.0)
 
     def test_priceWithMultipleQuantity(self):
         drink = Drinks("Diet Coke", 2)
         self.assertEqual(drink.getPrice(), 6)
+
+    def test_drinkChangeType(self):
+        drink = Drinks("Coke", 1)
+        drink.changeType("Diet Coke")
+        self.assertEqual(drink.getType(), "Diet Coke")
+        self.assertEqual(drink.getPrice(), 3)
+
+    def test_drinkChangeTypeMultipleQuantity(self):
+        drink = Drinks("Coke", 2)
+        drink.changeType("Diet Coke")
+        self.assertEqual(drink.getType(), "Diet Coke", 6)
+
+    def test_drinkChangeQuantity(self):
+        drink = Drinks("Coke", 1)
+        drink.changeQuantity(2)
+        self.assertEqual(drink.getPrice(), 5)
+
+
+class TestOrderClass(unittest.TestCase):
+
+    def test_init(self):
+        order = Order(1)
+        self.assertEqual(order.getItems(), [])
+        self.assertEqual(order.getOrderNumber(), 1)
+
+    def test_orderTotalcost(self):
+        drink = Drinks("Coke", 1)
+        pizza = Pizza("pepperoni", 12, 2)
+        order = Order(1)
+        order.addItem(drink)
+        order.addItem(pizza)
+        self.assertEqual(order.getTotalPrice(), 22.5)
+
+    def test_emptyorderTotalCost(self):
+        order = Order(1)
+        self.assertEqual(order.getTotalPrice(), 0)
+
+    def test_getOrderNumber(self):
+        order = Order(1)
+        self.assertEqual(order.getOrderNumber(), 1)
+
+    def test_addItem(self):
+        drink = Drinks("Coke", 1)
+        pizza = Pizza("pepperoni", 12, 2)
+        order = Order(1)
+        order.addItem(drink)
+        order.addItem(pizza)
+        self.assertEqual(order.getItems()[0].__dict__,
+                         {"type": "Coke", "price": 2.5, "quantity": 1, "category": "Drink"})
+        self.assertEqual(order.getItems()[1].__dict__,
+                         {"type": "pepperoni", "price": 20, "quantity": 2, "category": "Pizza", "size": 12,
+                          "topping": ['olives', 'tomatoes'], })
+
+    def test_jsonify(self):
+        drink = Drinks("Coke", 1)
+        order = Order(1)
+        order.addItem(drink)
+        self.assertEqual(order.jsonify(), '{"order_number": 1, "items": "[{\\"type\\": \\"Coke\\", \\"price\\": 2.5, \\"quantity\\": 1, \\"category\\": \\"Drink\\"}]"}')
